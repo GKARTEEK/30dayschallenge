57566dd6d62a81599f38a1a77f7cfa313eaeee96
GKarteek <2200030564@kluniversity.in>
2026-05-31 09:06:25 +0530

    Add login error message

--- accounts/views.py
+++ accounts/views.py
@@ -114,39 +114,36 @@ def register_view(request):
         }
     )
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# =========================
-# LOGIN
-# =========================
 
 def login_view(request):
 
     if request.method == 'POST':
 
-        username = request.POST['username']
-
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
         user = authenticate(
-
             request,
-
             username=username,
-
             password=password
         )
 
         if user is not None:
-
             login(request, user)
-
             return redirect('home')
 
-    return render(
-        request,
-        'login.html'
-    )
+        return render(
+            request,
+            'login.html',
+            {
+                'error': 'Incorrect username or password'
+            }
+        )
 
+    return render(request, 'login.html')
 
 # =========================
 # LOGOUT
